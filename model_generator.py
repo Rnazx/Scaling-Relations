e7a7467df2fc7038e3e771b1212fa9344535a304
Rion <rionazareth@gmail.com>
2023-06-14 16:22:58 +0530

    changes in conversion factor naming

--- model_generator.py
+++ model_generator.py
@@ -1,3 +1,4 @@
+#import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import *
@@ -10,7 +11,6 @@ sigma = Symbol('\Sigma')
 sigmatot = Symbol('Sigma_tot')
 sigmasfr = Symbol('Sigma_SFR')
 T = Symbol('T')
-
 
 # Defining the Constants
 calpha = Symbol('C_alpha')
@@ -25,7 +25,6 @@ delta = Symbol('\delta')
 mstar = Symbol('m_*')
 cl = Symbol('C_l')
 kappa = Symbol('kappa')
-zet = Symbol('zeta')
 E51 = Symbol('E_51')
 Rk = Symbol('R_k')
 kalpha = Symbol('K_alpha')
@@ -39,29 +38,25 @@ n = Symbol('n')
 nu = Symbol('nu')
 
 #conversion factors
-mpc = 1
-kpc = 1e+3*mpc
-kpcm = 3.086e+21
-pcm = kpcm/1e+3
-Msun = 1.989e+33
-cm = 1
-kpc = 3.086e+21*cm
-kms = 1e+5*cm
-Myr = 1e+6*(365*24*60*60)
+pc_kpc = 1e3  # number of pc in one kpc
+cm_kpc = 3.086e+21  # number of centimeters in one parsec
+cm_km = 1e5  # number of cm in one km
+s_Myr = 1e+6*(365*24*60*60)  # number of seconds in one megayear
+
 # Defining the expressions
 cs = (gamma*boltz*T/(mu*mh))**Rational(1/2)
 
 
-###############################################################################################
+###################################################################################################
 ###########################Function to generate the model###########################################
-#######################################################################################
+#################################################################################################
 def model_gen(model_no, let, tbool = True, alphareg = 1):
     def find_hlut(model_no, let):
         def choose_lreg(h, model_no):
             rho = sigma/(2*h)
             n = rho/((14/11)*mh)
             if [3, 4].count(model_no)>0 :
-                lsn = 0.14*kpc*(E51)**Fraction(16, 51) * (n/0.1)**Fraction(-19, 51)*(cs/(kms*10))**Fraction(-1, 3)
+                lsn = 0.14*cm_kpc*(E51)**Fraction(16, 51) * (n/0.1)**Fraction(-19, 51)*(cs/(cm_km*10))**Fraction(-1, 3)
                 l = (3/10)*lsn
                 l = simplify(l)
             else:
@@ -73,9 +68,9 @@ def model_gen(model_no, let, tbool = True, alphareg = 1):
         else:
             if let == 'a':
                 if model_no == 4:
-                    h = zet*cs/omega
+                    h = cs/omega
                 else: 
-                    h = zet*(cs**2)/(3*pi*G*sigmatot)
+                    h = (cs**2)/(3*pi*G*sigmatot)
                 nu = (delta*sigmasfr)/(2*h*mstar)
                 l, lsn, n = choose_lreg(h, model_no)
                 usn = ((4*pi/3)*l*lsn**3*cs**2*nu)**Fraction(1, 3)
@@ -90,16 +85,16 @@ def model_gen(model_no, let, tbool = True, alphareg = 1):
                 u = ((4*pi/3)*l*lsn**3*cs**2*(nu))**Fraction(1, 3)
                 usn = u.subs(h, 1)
                 if model_no == 4:
-                    h = simplify((zet*(usn)/(omega))**(1/(1-diff(log(u), h)*h)))
+                    h = simplify(((usn)/(omega))**(1/(1-diff(log(u), h)*h)))
                 else:
-                    h = simplify((zet*(usn**2)/(3*pi*G*sigmatot))**(1/(1-2*diff(log(u), h)*h)))
+                    h = simplify(((usn**2)/(3*pi*G*sigmatot))**(1/(1-2*diff(log(u), h)*h)))
                 l, lsn, n = choose_lreg(h, model_no)
                 nu = (delta*sigmasfr)/(2*h*mstar)
                 u = simplify(((4*pi/3)*l*lsn**3*cs**2*(nu))**Fraction(1, 3))
                 mach = u/cs
         taue = simplify(l/u)
         if model_no ==2: taur = taue
-        else : taur = simplify(6.8*Myr*(1/4)*(nu*kpc**3*Myr/50)**(-1)*(E51)**Fraction(-16, 17) * (n/0.1)**Fraction(19, 17)*(cs/(kms*10)))# does not work if model no = 2
+        else : taur = simplify(6.8*s_Myr*(1/4)*(nu*cm_kpc**3*s_Myr/50)**(-1)*(E51)**Fraction(-16, 17) * (n/0.1)**Fraction(19, 17)*(cs/(cm_km*10)))# does not work if model no = 2
         if model_no ==2 or tbool == False:
             tau = taue
         else :
@@ -147,98 +142,3 @@ def model_gen(model_no, let, tbool = True, alphareg = 1):
     quantities = [ h, l, u, tau, biso, bani, Bbar, tanpb,tanpbm, nu, n, cs, alphak, Dk, Dc, omega*tau,  kalpha*h/(calpha*tau*u), alphak1, alphak2, alphak3,  taue, taur]
 
     return quantities
-
-#########function for bining the data#####################################
-def bin_data(x, y, start, stop, step ):
-     bdata = []
-     for i in range(start, stop, step):
-         idx = np.where((x>i)*(x<i+2))
-         bdata.append(np.take(y,idx).mean())
-     return np.array(bdata)
-##############################Calibration Functions######################################
-def observable_model(model_no, let, not_ren, quanidx = 2):
-    quantities = model_gen(model_no, let, not_ren)
-    const = [(gamma, 1.5), (boltz, 1.3807e-16), (mh, 1.67e-24), (mu, 14/11), (mach, 1), (G, 6.67e-8),
-         (cl, 3/4), (xio, 0.4), (calpha, 1), (Rk, 0.3), (mstar, 0.85*Msun), (delta, 8e-3), (E51, 1)]
-
-    express = [quan.subs(const).simplify(force=True) for quan in quantities]
-
-    return express[quanidx]
-
-def forward_model(zetavals, exp):
-    an_vel = np.array(np.float64([exp.evalf(subs={ sigmatot:sigt, sigma: sig, sigmasfr: sigsfr, q:qs, omega: oms, zet:zetaval}) for sigt,sig, sigsfr,qs, oms, zetaval in zip(dat_sigmatot, dat_sigma, dat_sigmasfr, dat_q, dat_omega, zetavals)]))
-    return an_vel
-
-
-def parameter_calib(model_no, let, zetavals, bin_vel, not_ren = True, quanidx = 2, parstop = 100000, parstep = 1000):
-    rms = []
-    par_space = np.arange(1, parstop, parstep)
-    express = observable_model(model_no, let, not_ren, quanidx)
-    for alpha in par_space:
-        rms.append(np.sqrt(((bin_vel-forward_model(alpha*zetavals, express))**2).mean()))
-    rms = np.array(rms)
-    alp_min = par_space[np.argmin(rms)]
-    return alp_min*zetavals
-###############################################################################################
-###########################Function to derive the model in terms of observables###########################################
-#######################################################################################
-def analytical_data(model_no, let, ren, alphar, z, t, ca, rk):
-    quantities = model_gen(model_no, let, ren, alphar)
-
-    const = [(gamma, 1.5), (boltz, 1.3807e-16), (mh, 1.67e-24), (mu, 14/11), (mach, 1), (G, 6.67e-8),
-            (cl, 3/4), (xio, 0.4), (calpha, ca), (Rk, rk), (mstar, 0.85*Msun), (delta, 8e-3), (E51, 1), (kalpha, 1)]
-    variables = [(sigmatot, 1), (sigma, 1),
-                (sigmasfr, 1), (omega, 1), (q, 1), (T, 1)]
-
-    # plotting the scaling relations
-    observ = [sigmatot, sigma, sigmasfr, q, omega, T]
-
-    for obs in observ:
-        variables.remove((obs, 1))
-    final = const + variables
-
-    express = [quan.subs(final).simplify(force=True) for quan in quantities]
-
-    zetaval = z
-    opt_temp = t 
-    scalreldata = []
-    for exp in express:
-        scalreldata.append([exp.evalf(subs={ sigmatot:sigt, sigma: sig, sigmasfr: sigsfr, q:qs, omega: oms, zet:zets, T:t}) for sigt,sig, sigsfr,qs, oms,zets, t in zip(dat_sigmatot, dat_sigma, dat_sigmasfr, dat_q, dat_omega,zetaval, opt_temp)])
-    scalreldata = np.array(scalreldata)
-    scalreldata[6] = scalreldata[6]*(np.float64(scalreldata[6]*scalreldata[6]>0))
-    scalreldata = np.float64(scalreldata)
-
-    return scalreldata
-###################################################################################################################
-#Data in the same radius###############################
-from scipy.interpolate import griddata
-def data_conv(data):
-    kpc_r_surf = data[0]* kpc_D_M31_Plot / kpc_D_M31_Chemin
-    dat_sigmatot = data[1]*Msun/(pcm)**2
-    dat_sigma = data[2]*Msun/(pcm)**2
-    kpc_r_SFR = data[3] * kpc_D_M31_Plot / kpc_D_M31_TB10
-    dat_sigmasfr = data[4]*Msun/((10**9*365*24*60*60)*(pcm)**2)
-    kmskpc_Om = data[5]/kpc_r_surf
-    dat_omega = kmskpc_Om*1e+5/kpcm
-    dat_q = -1 * kpc_r_surf / kmskpc_Om * np.gradient(kmskpc_Om)/np.gradient(kpc_r_surf)
-    kpc_r_molfrac = data[6] / arcmin_deg / deg_rad * kpc_D_M31_Plot
-    molfrac = data[7]
-    kpc_r_cl = data[8]
-    return kpc_r_cl, kpc_r_surf, dat_sigmatot, dat_sigma, dat_q, dat_omega, kpc_r_SFR, dat_sigmasfr,   kpc_r_molfrac, molfrac
-
-def interp_bin(data):
-    kpc_r_cl, kpc_r_surf, dat_sigmatot, dat_sigma, dat_q, dat_omega, kpc_r_SFR, dat_sigmasfr,   kpc_r_molfrac, molfrac = data_conv(data)
-    rad_data = [kpc_r_cl, kpc_r_surf, kpc_r_SFR, kpc_r_molfrac]
-    # observ_data = [dat_sigmatot, dat_sigma, dat_q, dat_omega, dat_sigmasfr, molfrac]
-    kpc_r = rad_data[np.argmin(np.array([d.size for d in rad_data]))]
-
-    dat_sigmatot = griddata(kpc_r_cl, dat_sigmatot, kpc_r, method='linear', fill_value=nan, rescale=False)
-    dat_sigma = griddata(kpc_r_surf, dat_sigma, kpc_r, method='linear', fill_value=nan, rescale=False)
-    dat_q = griddata(kpc_r_surf, dat_q, kpc_r, method='linear', fill_value=nan, rescale=False)
-    dat_omega = griddata(kpc_r_surf, dat_omega, kpc_r, method='linear', fill_value=nan, rescale=False)
-
-    dat_sigmasfr = griddata(kpc_r_SFR, dat_sigmasfr, kpc_r, method='linear', fill_value=nan, rescale=False)
-    molfrac = griddata(kpc_r_molfrac, molfrac, kpc_r, method='linear', fill_value=nan, rescale=False)
-
-    return  kpc_r, dat_sigmatot, dat_sigma, dat_q, dat_omega, dat_sigmasfr, molfrac 
-
