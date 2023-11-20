7edb355c4ac2be82eaf3dca98480a09d3d4e5e25
Rion <riong.nazareth@niser.ac.in>
2023-11-20 11:27:50 +0530

    reading from file

---
+++ regimes.py
@@ -0,0 +1,25 @@
+'''
+The regime for h can be any one of the following: copy any one of them and paste it in value for hreg variable
+'subsonic', 'supersonic', 'cs/omega'
+'''
+hreg='subsonic'
+'''
+The regime for l can be any one of the following: copy any one of them and paste it in value for lreg variable
+'maximum scale-driven', 'isolated supernovae-driven', 'superbubble-driven'
+'''
+lreg='isolated supernovae-driven'
+'''
+The regime for u can be any one of the following: copy any one of them and paste it in value for ureg variable
+'sound speed', 'supenovae/superbubble-driven'
+'''
+ureg='supenovae/superbubble-driven'
+'''
+The regime for tau can be any one of the following: copy any one of them and paste it in value for taureg variable
+'eddy turnover time', 'supernovae/superbubble renovation time'
+'''
+taureg='eddy turnover time'
+'''
+The regime for tau can be any one of the following: copy any one of them and paste it in value for alphareg variable
+'regime 1', 'regime 2', 'regime 3'
+'''
+alphareg='regime 1'
